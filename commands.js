5404262238fc1d4daa8ef0030b23a59aa4fc3bd2
Barlog <championhsia@gmail.com>
2026-03-02 23:28:48 +0800

    新增標器人看到標註內容，才進行回應、執行指令

--- commands.js
+++ commands.js
@@ -2,6 +2,7 @@ import { gif } from "./commands/gif.js";
 import { Lin } from "./commands/Lin.js";
 import { restart } from "./commands/restart.js";
 
+
 const commands = {
     Lin,
     gif,
@@ -15,23 +16,28 @@ export async function gotMessage(msg) {
 
     // 表示有收到訊息
     console.log(`收到訊息: ${msg.content}`);
-    
+
+
+    // 讀取標註機器人的訊息
+    const botMention = new RegExp(`^<@!?${msg.client.user.id}>`);
+
     // 設定機器人在特定頻道活動
-    if (msg.channel.id == process.env.CHANNELID){
+    if (msg.channel.id == process.env.CHANNELID && botMention.test(msg.content)) {
 
         // 初步擷取訊息並且設定指令類型
+        msg.content = msg.content.replace(botMention, "").trim();
         let tokens = msg.content.split(" ");
         let command = tokens.shift();
 
         // 正式辨識指令類類並執行
-        try{
-            if (command.charAt(0) === "!"){
-                command = command.substring(1); 
+        try {
+            if (command.charAt(0) === "!") {
+                command = command.substring(1);
                 commands[command](msg, tokens);
-            } 
-        } catch(error){
+            }
+        } catch (error) {
             // 報錯並且檢視錯誤資訊
-                console.error("❌ 發生錯誤了：", error.message);
+            console.error("❌ 發生錯誤了：", error.message);
         }
     }
 }
